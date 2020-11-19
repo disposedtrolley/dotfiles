1530cb447955a2b9e6b2d9d838d4c016e5df6136
James Liu <james@jamesliu.io>
2020-11-19 17:57:02 +1100

    feat: add goenv to fish config

--- shell/config.fish
+++ shell/config.fish
@@ -1,3 +1,9 @@
 set -Ux EDITOR nvim
 
+# Go
+set -x GOENV_ROOT $HOME/.goenv
+set -gx PATH $GOENV_ROOT/bin $PATH
+source (goenv init - | psub) # https://stanislas.blog/2018/07/how-to-use-nvm-rbenv-pyenv-goenv-with-fish-shell/
+
+# UI
 theme_gruvbox dark medium
