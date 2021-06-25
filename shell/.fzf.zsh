31c0ad64b20bdc71537e2616ae47825b3e0b7cad
James Liu <james@jamesliu.io>
2021-06-25 20:32:42 +1000

    update fzf config

--- shell/.fzf.zsh
+++ shell/.fzf.zsh
@@ -1,19 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
-
-# Default behaviour
-export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!.git/*"'
-
-# fzf with preview
-alias fzfp=fzf --preview 'bat --style=numbers --color=always --line-range :500 {}'
+source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
